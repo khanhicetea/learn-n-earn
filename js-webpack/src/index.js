d543b28987f6c9563ed0a967116e1b190c5ea949
khanhicetea <khanhicetea@gmail.com>
2017-10-05 09:41:36 +0700

    clean up for output management

--- js-webpack/src/index.js
+++ js-webpack/src/index.js
@@ -1,20 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import DogImage from './dog.jpg';
-import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div');
 
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.innerHTML += _.join([', from', Data.note.from], ' ');
-  element.classList.add('hello');
-
-  var cuteDog = new Image();
-  cuteDog.src = DogImage;
-
-  element.appendChild(cuteDog);
 
   return element;
 }
